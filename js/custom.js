ada9a88e8631c064f5f9f6817a1e22913c845946
mumtaz113 <mumtazahmad5122@gmail.com>
2024-03-25 09:45:17 +0500

    Final Changes Done

--- js/custom.js
+++ js/custom.js
@@ -125,8 +125,8 @@ $('.brand-blk').slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 1,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
